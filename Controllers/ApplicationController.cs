fccb5ddb9fad5e8800aa0bdaaae72d829613b22a
Darshan <brunodarshan@gmail.com>
2020-03-25 11:34:54 -0300

    first commit

---
+++ Controllers/ApplicationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DailyClass.Controllers
+{
+    [ApiController]
+    [Route("/api")]
+    public class ApplicationController : ControllerBase
+    {
+        [Route("")]
+        public string Index(){
+            return "GET index";
+        }
+    }
+}
